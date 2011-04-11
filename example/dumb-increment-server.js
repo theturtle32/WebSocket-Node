11544eaabb49fa32021578a8cf532c5cf80ad850
Brian McKelvey <brian@worlize.com>
2011-04-11 01:53:25 -0700

    Too many changes to enumerate.  :-P

--- example/dumb-increment-server.js
+++ example/dumb-increment-server.js
@@ -1,48 +1,55 @@
+#!/usr/bin/env node
+
 var WebSocketServer = require('../lib/WebSocketServer');
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
-    console.log("Received request for " + request.url);
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    response.end("Hello World");
+    console.log((new Date()) + " Received request for " + request.url);
+    if (request.url == "/") {
+        fs.readFile('increment-test.html', 'utf8', function(err, data) {
+            if (err) {
+                response.writeHead(404);
+                response.end();
+            }
+            else {
+                response.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                response.end(data);
+            }
+        });
+    }
+    else {
+        response.writeHead(404);
+        response.end();
+    }
 });
 server.listen(8080, function() {
-    console.log("Server is listening on port 8080");
+    console.log((new Date()) + " Server is listening on port 8080");
 });
 
 wsServer = new WebSocketServer(server, null, 'dumb-increment-protocol');
 
-// wsServer.on('request', function(request) {
-//     var origin = url.parse(request.origin);
-//     if (/worlize.com$/.test(origin.hostname)) {
-//         var connection = request.accept('lws-mirror-protocol', request.origin);
-//         console.log("Connection accepted");
-//     }
-//     else {
-//         request.reject(400, "Origin not allowed");
-//     }
-// });
-
 wsServer.on('connection', function(connection) {
-    console.log("Connection accepted.");
+    console.log((new Date()) + " Connection accepted from " + connection.remoteAddress);
     var number = 0;
     connection.timerInterval = setInterval(function() {
         connection.sendUTF((number++).toString(10));
-    }, 50);
-    connection.socket.on('end', function() {
+    }, 0);
+    connection.on('close', function() {
         clearInterval(connection.timerInterval);
-        console.log("Connection end");
     });
-    connection.socket.on('close', function(hadError) {
-        clearInterval(connection.timerInterval);
-        if (hadError) {
-            console.log("Connection closed with an error.");
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            if (message.utf8Data === 'reset\n') {
+                console.log((new Date()) + " Reset received");
+                number = 0;
+            }
         }
-        else {
-            console.log("Connection closed");
-        }
+    });
+    connection.on('close', function(connection) {
+        console.log((new Date()) + " Client " + connection.remoteAddress + " disconnected");
     });
 });
--- example/echo-server.js
+++ example/echo-server.js
@@ -1,16 +1,33 @@
+#!/usr/bin/env node
+
 var WebSocketServer = require('../lib/WebSocketServer');
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
-    console.log("Received request for " + request.url);
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    response.end("Hello World");
+    console.log((new Date()) + " Received request for " + request.url);
+    if (request.url == "/") {
+        fs.readFile('mirror-test.html', 'utf8', function(err, data) {
+            if (err) {
+                response.writeHead(404);
+                response.end();
+            }
+            else {
+                response.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                response.end(data);
+            }
+        });
+    }
+    else {
+        response.writeHead(404);
+        response.end();
+    }
 });
 server.listen(8080, function() {
-    console.log("Server is listening on port 8080");
+    console.log((new Date()) + " Server is listening on port 8080");
 });
 
 wsServer = new WebSocketServer(server, null, 'lws-mirror-protocol');
@@ -18,8 +35,7 @@ wsServer = new WebSocketServer(server, null, 'lws-mirror-protocol');
 // wsServer.on('request', function(request) {
 //     var origin = url.parse(request.origin);
 //     if (/worlize.com$/.test(origin.hostname)) {
-//         var connection = request.accept('lws-mirror-protocol', request.origin);
-//         console.log("Connection accepted");
+//         request.accept('lws-mirror-protocol', request.origin);
 //     }
 //     else {
 //         request.reject(400, "Origin not allowed");
@@ -29,27 +45,20 @@ wsServer = new WebSocketServer(server, null, 'lws-mirror-protocol');
 var connections = [];
 
 wsServer.on('connection', function(connection) {
-    console.log("Connection accepted without request handler.");
+    console.log((new Date()) + " Connection accepted.");
     connections.push(connection);
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-            console.log(message.utf8Data);
+            // console.log((new Date()) + " " + message.utf8Data);
             connections.forEach(function (outputConnection) {
                 outputConnection.sendUTF(message.utf8Data);
             });
         }
     });
-    connection.socket.on('end', function() {
+    connection.on('close', function(connection) {
         var index = connections.indexOf(connection);
         if (index !== -1) {
-            console.log("Connection closed... removing connection at index " + index);
-            connections.splice(index, 1);
-        }
-    });
-    connection.socket.on('close', function() {
-        var index = connections.indexOf(connection);
-        if (index !== -1) {
-            console.log("Connection closed... removing connection at index " + index);
+            console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
             connections.splice(index, 1);
         }
     });
