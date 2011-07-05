bb74e02145eea9af6e554366568d6bba8a24abd0
Brian McKelvey <brian@worlize.com>
2011-07-05 16:54:16 -0700

    Adding index.js and package.json for npm.

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "server": require('./lib/WebSocketServer'),
+    "client": require('./lib/WebSocketClient'),
+    "router": require('./lib/WebSocketRouter'),
+    "frame":  require('./lib/WebSocketFrame')
+};
