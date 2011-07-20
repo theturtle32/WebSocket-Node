683989d5586d58753eb813b3618500253d9b6f1e
Brian McKelvey <brian@worlize.com>
2011-07-19 18:20:04 -0700

    Exposing WebSocketRequest in the public API to support Socket.IO integration.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     "server": require('./lib/WebSocketServer'),
     "client": require('./lib/WebSocketClient'),
     "router": require('./lib/WebSocketRouter'),
-    "frame":  require('./lib/WebSocketFrame')
+    "frame":  require('./lib/WebSocketFrame'),
+    "request": require('./lib/WebSocketRequest')
 };
