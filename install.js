0ca1043a5bc8ac67160b6b5a94e720a003aee739
Brian McKelvey <brian@worlize.com>
2012-09-03 01:58:52 -0700

    Adding clarifying information for build failure error message to let users know that the module will still work.

--- install.js
+++ install.js
@@ -17,6 +17,9 @@ gyp.on('exit', function(code) {
     if (code !== 0) {
         console.log("[websocket v%s]", version);
         console.log('    Native code compile failed!!');
+        console.log('    Please note that this module DOES NOT REQUIRE the native components');
+        console.log('    and will still work without them, though not quite as efficiently.');
+        console.log('');
         console.log('    On Windows, native extensions require Visual Studio and Python.');
         console.log('    On Unix, native extensions require Python, make and a C++ compiler.');
         console.log('    Start npm with --websocket:verbose to show compilation output (if any).');
