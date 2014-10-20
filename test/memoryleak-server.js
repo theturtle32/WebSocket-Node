13013e946038afa222535c40479883127f4c6053
Brian McKelvey <brian@worlize.com>
2014-10-19 23:38:23 -0700

    Adding a test client/server to check for memory leaks (and a few other things).  See issue #105

---
+++ test/memoryleak-server.js
@@ -0,0 +1,54 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+var heapdump = require('heapdump');
+var memwatch = require('memwatch');
+var fs = require('fs');
+var WebSocketServer = require('../lib/websocket').server;
+var https = require('https');
+
+var activeCount = 0;
+
+var config = { 
+    key: fs.readFileSync( "privatekey.pem" ), 
+    cert: fs.readFileSync( "certificate.pem" )  
+};
+
+var server = https.createServer( config );
+
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080 (wss)');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false    
+});
+
+wsServer.on('request', function(request) {
+    activeCount++;
+    console.log("Opened from: %j\n---activeCount---: %d", request.remoteAddresses, activeCount);
+    var connection = request.accept(null, request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }       
+    });
+    connection.on('close', function(reasonCode, description) {
+        activeCount--;
+        console.log("Closed. (" + reasonCode + ") " + description +
+                    "\n---activeCount---: " + activeCount);
+    });
+    connection.on('error', function(error) {
+        console.log("Connection error: " + error);
+    });
+});
+
+setInterval( function(){
+    // global.gc();
+    var filename = './heapdump/'+ new Date().getTime() + '_' + activeCount + '.heapsnapshot';
+    console.log("Triggering heapdump to write to %s", filename);
+    heapdump.writeSnapshot( filename );    
+}, 10000 );
+memwatch.on('leak', function(info) { console.log(info); });
