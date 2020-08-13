996f3cc355eb417b48bc7e976bd8afbf92db3539
c calitz <cordell.calitz@safetyio.com>
2020-08-13 14:49:18 +0200

    SMC-728 - fix error handler in WebSocketRequest.js & add unit test

---
+++ test/unit/requestErrorHandler.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+var test = require('tape');
+var WebSocket = require('../../lib/W3CWebSocket');
+var WebSocketServer = require('../../lib/WebSocketServer');
+var http = require('http');
+var port = 8080;
+
+test('W3CWebSockets adding event listeners with ws.onxxxxx', function(t) {
+  var server = http.createServer(function(request, response) {
+    if (debug) { console.log((new Date()) + ' Received request for ' + request.url); }
+    response.writeHead(404);
+    response.end();
+  });
+  server.listen(port, function() {
+    console.log((new Date()) + ' Server is listening on port ' + port);
+  });
+
+  var wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false,
+    maxReceivedFrameSize: 64*1024*1024,   // 64MiB
+    maxReceivedMessageSize: 64*1024*1024, // 64MiB
+    fragmentOutgoingMessages: false,
+    keepalive: false,
+    disableNagleAlgorithm: false
+  });
+
+  wsServer.on('request', function(carrier){
+    // force a write after destroyed error
+    carrier.socket.destroy();
+    carrier.reject();
+  });
+
+  var ws = new WebSocket('ws://localhost:8080/');
+
+  ws.onerror = function(event) {
+    t.pass('Connection should not be successful');
+  };
+
+  setTimeout(()=>{
+    t.pass('process should still be running');
+    ws.close();
+    server.close();
+    t.end();
+  }, 100);
+});
